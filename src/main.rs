741a6bd6ad347ebf4aa6baa70532b6e63a6bec11
HD-29139 <santanajefferson594@gmail.com>
2025-04-18 02:32:51 -0300

    structs

--- src/main.rs
+++ src/main.rs
@@ -7,79 +7,101 @@ fn time_to_travel(distance: u64) -> f64 {
     distance as f64 / SPEED_OF_LIGHT as f64
 }
 fn main() {
-    const FIVE_HOURS_IN_SECONDS: u32 = 60 * 60 * 5;
-    const PI: f64 = 3.14159;
-    const CIRCUMFERENCE: f64 = 2.0 * PI * 5.0;
+    // const FIVE_HOURS_IN_SECONDS: u32 = 60 * 60 * 5;
+    // const PI: f64 = 3.14159;
+    // const CIRCUMFERENCE: f64 = 2.0 * PI * 5.0;
 
-    // println!("{FIVE_HOURS_IN_SECONDS}");
-    // println!("{PI}");
-    // println!("{CIRCUMFERENCE}");
+    // // println!("{FIVE_HOURS_IN_SECONDS}");
+    // // println!("{PI}");
+    // // println!("{CIRCUMFERENCE}");
 
-    let distance = 1_000_000_000;
-    let time = time_to_travel(distance);
-    // println!("Time to travel {distance} meters at the speed of light: {time:.6} seconds");
+    // let distance = 1_000_000_000;
+    // let time = time_to_travel(distance);
+    // // println!("Time to travel {distance} meters at the speed of light: {time:.6} seconds");
 
-    let spaces = "hello";
-    let spaces = 8;
+    // let spaces = "hello";
+    // let spaces = 8;
 
-    // println!("{spaces}");
+    // // println!("{spaces}");
 
-    let quotient = 56.7 / 32.2;
-    let truncated: i32 = -5 / 3;
+    // let quotient = 56.7 / 32.2;
+    // let truncated: i32 = -5 / 3;
 
-    let rm = 40 % 5;
+    // let rm = 40 % 5;
 
-    // println!("{rm}")
+    // // println!("{rm}")
 
-    let tup = (500, 56.6, 1);
-    let (x, y, z) = tup;
+    // let tup = (500, 56.6, 1);
+    // let (x, y, z) = tup;
 
-    // println!("{y}");
+    // // println!("{y}");
 
-    let five_hundred = tup.0;
-    let one = tup.2;
+    // let five_hundred = tup.0;
+    // let one = tup.2;
 
-    // println!("{one} and {five_hundred}");
+    // // println!("{one} and {five_hundred}");
 
-    let a: [i32; 5] = [1, -5, 3, 4, 5];
+    // let a: [i32; 5] = [1, -5, 3, 4, 5];
 
-    // let a = [10; 5];
+    // // let a = [10; 5];
 
-    // println!("{}", a[1]);
+    // // println!("{}", a[1]);
 
-    
-    loop {
-        let mut index = String::new();
+    // loop {
+    //     let mut index = String::new();
 
-        println!("input the index value:");
-        io::stdin().read_line(&mut index).expect("failed to read");
-        let index: usize = match index.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please input a valid number.");
-                continue;
-            }
-        };
+    //     println!("input the index value:");
+    //     io::stdin().read_line(&mut index).expect("failed to read");
+    //     let index: usize = match index.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => {
+    //             println!("Please input a valid number.");
+    //             continue;
+    //         }
+    //     };
 
-        if index >= a.len(){
-            println!("please input a smaller value");
-            continue;
-        }
-        else {
-            let element = a[index];
-            println!("The value of the element at index {index} is: {element}");
-            break;
+    //     if index >= a.len() {
+    //         println!("please input a smaller value");
+    //         continue;
+    //     } else {
+    //         let element = a[index];
+    //         println!("The value of the element at index {index} is: {element}");
+    //         break;
+    //     }
+    // }
+
+    // let a = [10, 20, 30, 40, 50];
+
+    // for element in a {
+    //     println!("the value is: {element}");
+    // }
+
+    // for number in (1..=4).rev() {
+    //     println!("{number}");
+    // }
+
+    struct Person {
+        name: String,
+        age: i32,
+        is_alive: bool,
+    }
+
+    let p1 = Person {
+        name: String::from("Mae Mann"),
+        age: 43,
+        is_alive: false,
+    };
+
+    fn personBuilder(name: String, age: i32, is_alive: bool) -> Person {
+        Person {
+            name,
+            age,
+            is_alive,
         }
     }
 
-        let a = [10, 20, 30, 40, 50];
-    
-        for element in a {
-            println!("the value is: {element}");
-        }
+    let p2 = personBuilder(String::from("Alejandro Hines"), 37, true);
 
-        for number in (1..=4).rev() {
-            println!("{number}");
-        }
-    
+    println!("Name: {} Age: {} Is alive: {}", p1.name, p1.age, p1.is_alive);
+        
 }
